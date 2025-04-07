5f6b0ca8db99dc04b2714c1d74dc0899f3f3bb5a
Steve Collison <collison.steven@gmail.com>
2025-04-07 14:04:56 -0400

    update to css

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: '*' }
-});
+const io = new Server(server, { cors: { origin: '*' } });
 
 const sessions = {};
 
@@ -16,11 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/create-session', (req, res) => {
   const sessionId = nanoid(6);
-  sessions[sessionId] = {
-    users: {},
-    votesRevealed: false,
-    admin: null
-  };
+  sessions[sessionId] = { users: {}, votesRevealed: false, admin: null };
   res.json({ sessionId });
 });
 
@@ -35,20 +29,14 @@ io.on('connection', (socket) => {
     session.users[socket.id] = { name, vote: null, isAdmin };
     socket.join(sessionId);
     socket.sessionId = sessionId;
-    io.to(sessionId).emit('state', {
-      users: session.users,
-      votesRevealed: session.votesRevealed
-    });
+    io.to(sessionId).emit('state', { users: session.users, votesRevealed: session.votesRevealed });
   });
 
   socket.on('vote', (value) => {
     const session = sessions[socket.sessionId];
     if (session && session.users[socket.id]) {
       session.users[socket.id].vote = value;
-      io.to(socket.sessionId).emit('state', {
-        users: session.users,
-        votesRevealed: session.votesRevealed
-      });
+      io.to(socket.sessionId).emit('state', { users: session.users, votesRevealed: session.votesRevealed });
     }
   });
 
@@ -57,10 +45,7 @@ io.on('connection', (socket) => {
     if (!session) return;
     if (session.users[socket.id]?.isAdmin) {
       session.votesRevealed = true;
-      io.to(socket.sessionId).emit('state', {
-        users: session.users,
-        votesRevealed: true
-      });
+      io.to(socket.sessionId).emit('state', { users: session.users, votesRevealed: true });
     }
   });
 
@@ -70,10 +55,7 @@ io.on('connection', (socket) => {
     if (session.users[socket.id]?.isAdmin) {
       Object.values(session.users).forEach(u => u.vote = null);
       session.votesRevealed = false;
-      io.to(socket.sessionId).emit('state', {
-        users: session.users,
-        votesRevealed: false
-      });
+      io.to(socket.sessionId).emit('state', { users: session.users, votesRevealed: false });
     }
   });
 
@@ -84,10 +66,7 @@ io.on('connection', (socket) => {
       if (Object.keys(session.users).length === 0) {
         delete sessions[socket.sessionId];
       } else {
-        io.to(socket.sessionId).emit('state', {
-          users: session.users,
-          votesRevealed: session.votesRevealed
-        });
+        io.to(socket.sessionId).emit('state', { users: session.users, votesRevealed: session.votesRevealed });
       }
     }
   });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  content: [
-    './public/**/*.html',    // Make sure to scan all HTML files
-    './public/**/*.js',      // Make sure to scan JavaScript files if you're using Tailwind classes dynamically
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
+  content: ["./public/**/*.html"],
+  theme: { extend: {} },
   plugins: [],
 };
