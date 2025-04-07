2a68f9e92fe05f33eb0513c2c4e5bc654ac8fe8f
Steve Collison <collison.steven@gmail.com>
2025-04-07 13:26:01 -0400

    output
    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: [
+    './public/**/*.html',    // Make sure to scan all HTML files
+    './public/**/*.js',      // Make sure to scan JavaScript files if you're using Tailwind classes dynamically
+  ],
   darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
