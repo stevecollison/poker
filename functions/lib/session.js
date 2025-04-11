d205d6e00cd0bb2c865b6d900cf668b0e475e44c
Steve Collison <collison.steven@gmail.com>
2025-04-11 11:38:22 -0400

    session fix

--- functions/lib/session.js
+++ functions/lib/session.js
@@ -1,19 +1,18 @@
-import { Redis } from '@upstash/redis/cloudflare';
+import { Redis } from '@upstash/redis';
 
 export function getSessionClient(env) {
   return new Redis({
     url: env.UPSTASH_REDIS_REST_URL,
-    token: env.UPSTASH_REDIS_REST_TOKEN,
+    token: env.UPSTASH_REDIS_REST_TOKEN
   });
 }
 
-export async function getSession(sessionId, env) {
+export async function getSession(env, sessionId) {
   const redis = getSessionClient(env);
-  const result = await redis.get(sessionId);
-  return result || { users: [], value: null, revealed: false };
+  return await redis.get(`session:${sessionId}`);
 }
 
 export async function saveSession(env, sessionId, session) {
-    const redis = getSessionClient(env);
-    await redis.set(`session:${sessionId}`, session, { ex: 86400 });
-  }
+  const redis = getSessionClient(env);
+  await redis.set(`session:${sessionId}`, session, { ex: 86400 });
+}
